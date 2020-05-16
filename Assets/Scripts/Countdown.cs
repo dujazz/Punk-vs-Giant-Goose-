803ba213536a81b369ca1731cdbce56cf71f8506
Denis Uspensky <dujazz88@gmail.com>
2020-05-16 14:20:32 +0300

    meta commit&

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    public void SetCountdownStart()
+    {
+        GameManager.Instance.countdownDone = false;
+    }
+    public void SetCountdownDone()
+    {
+        GameManager.Instance.countdownDone = true;
+    }
+}
---
+++ Assets/Scripts/WallOfFire.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallOfFire : MonoBehaviour
+{
+    [SerializeField] int damage = 1;
+}
