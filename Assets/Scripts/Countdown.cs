19c03bd15403d2bcb4e6d93fda376e40b944ecfb
Denis Uspensky <dujazz88@gmail.com>
2020-05-17 21:41:08 +0300

    Countdown audio added Made some environment

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class Countdown : MonoBehaviour
 {
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] List <AudioClip> audioClips;
+    int activeAudioClipIndex = 0;
+
+
     public void SetCountdownStart()
     {
         GameManager.Instance.countdownDone = false;
     }
+
     public void SetCountdownDone()
     {
         GameManager.Instance.countdownDone = true;
     }
+
+    public void CountdownAudio()
+    {
+        audioSource.PlayOneShot(audioClips[activeAudioClipIndex]);
+
+        if (activeAudioClipIndex == audioClips.Count - 1)
+        {
+            activeAudioClipIndex = 0;
+        }
+        else
+        {
+            activeAudioClipIndex++;
+        }
+    }
+
 }
